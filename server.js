2c0ae3bb3e684a09bb924aa3c3d366dd3b3c5ed3
Chris Gorski <c.gorski82@gmail.com>
2020-01-10 00:32:07 -0500

    changed drawing to path creation and added socket functionality

--- server.js
+++ server.js
@@ -16,10 +16,17 @@ app.route('/')
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+    socket.on('sendDrawing', function (data) {
+        console.log('sending drawing');
+        io.emit('receiveDrawing', data);
+    })
+
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 });
+
+
   
 //404 Not Found Middleware
 app.use(function(req, res, next) {
@@ -31,27 +38,6 @@ app.use(function(req, res, next) {
   //Start our server and tests!
 http.listen(port , function () {
     console.log("Listening on port " + port);
-    // if(process.env.NODE_ENV==='test') {
-    //   console.log('Running Tests...');
-    //   setTimeout(function () {
-    //     try {
-    //       runner.run();
-    //     } catch(e) {
-    //       var error = e;
-    //         console.log('Tests are not valid:');
-    //         console.log(error);
-    //     }
-    //   }, 1500);
-    //}
   });
   
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
